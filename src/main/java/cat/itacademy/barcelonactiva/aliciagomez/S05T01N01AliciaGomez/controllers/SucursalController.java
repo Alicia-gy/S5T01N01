44f507523b7458d4b3b30f27d51616efdb2fca71
Alicia <alicia.gomez.y@gmail.com>
2024-01-17 13:51:32 +0100

    html added

--- src/main/java/cat/itacademy/barcelonactiva/aliciagomez/S05T01N01AliciaGomez/controllers/SucursalController.java
+++ src/main/java/cat/itacademy/barcelonactiva/aliciagomez/S05T01N01AliciaGomez/controllers/SucursalController.java
@@ -1,23 +1,14 @@
 package cat.itacademy.barcelonactiva.aliciagomez.S05T01N01AliciaGomez.controllers;
 
 import java.util.List;
-import java.util.Optional;
 
+import cat.itacademy.barcelonactiva.aliciagomez.S05T01N01AliciaGomez.model.dto.SucursalDTO;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
-import cat.itacademy.barcelonactiva.aliciagomez.S05T01N01AliciaGomez.model.domain.Sucursal;
 import cat.itacademy.barcelonactiva.aliciagomez.S05T01N01AliciaGomez.model.services.SucursalService;
 
 @Controller
@@ -29,59 +20,58 @@ public class SucursalController {
 
     @GetMapping("/")
     public String index(Model model){
-        model.addAttribute("titulo", "Indice");
+        model.addAttribute("listSucursalDTO", sucursalService.findAll());
         return "index";
     }
 
     //create new
     @PostMapping("/add")
-    public ResponseEntity<?> create (@RequestBody Sucursal sucursal){
-        return ResponseEntity.status(HttpStatus.CREATED).body(sucursalService.save(sucursal));
+    public String create (@ModelAttribute SucursalDTO sucursalDTO){
+        sucursalService.save(sucursalDTO);
+        return "redirect:/sucursal/";
     }
+
+    @GetMapping("/showNewSucursalForm")
+    public String showNewSucursalForm(Model model) {
+        // create model attribute to bind form data
+        SucursalDTO dto = new SucursalDTO();
+        model.addAttribute("sucursal", dto);
+        return "add-sucursal";
+    }
+
 
     //read by id
     @GetMapping("/getOne/{id}")
-    public ResponseEntity<?> read(@PathVariable(value = "id") int id){
-        Optional<Sucursal> sucursal = sucursalService.findById(id);
+    public String read(@PathVariable(value = "id") int id, Model model) {
+        SucursalDTO dto = sucursalService.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
 
-        if(!sucursal.isPresent()) {
-            return ResponseEntity.notFound().build();
-        }
-
-        return ResponseEntity.ok(sucursal);
+        model.addAttribute("sucursal", dto);
+        return "update-sucursal";
     }
 
     //read all
     @GetMapping("/getAll")
-    public List<Sucursal> readAll() {
+    public List<SucursalDTO> readAll() {
         return sucursalService.findAll();
     }
 
     //update
     @PutMapping("/update/{id}")
-    public ResponseEntity<?> update(@RequestBody Sucursal SucursalDetails, @PathVariable(value = "id") int id) {
-        Optional<Sucursal> sucursal = sucursalService.findById(id);
+    public String update(@PathVariable(value = "id") int id, Model model) {
+        SucursalDTO dto = sucursalService.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
 
-        if(!sucursal.isPresent()) {
-            return ResponseEntity.notFound().build();
-        }
-
-        sucursal.get().setNom(SucursalDetails.getNom());
-        sucursal.get().setPais(SucursalDetails.getPais());
-
-        return ResponseEntity.status(HttpStatus.CREATED).body(sucursalService.save(sucursal.get()));
-
+        model.addAttribute("user", dto);
+        return "update-user";
     }
 
     //delete
     @DeleteMapping("/delete/{id}")
-    public ResponseEntity<?> delete(@PathVariable(value = "id") int id){
-        if(!sucursalService.findById(id).isPresent()) {
-            return ResponseEntity.notFound().build();
-        }
+    public String delete(@PathVariable(value = "id") int id){
 
         sucursalService.deleteById(id);
-        return ResponseEntity.ok().build();
+        return "redirect:/sucursal/";
 
     }
 
